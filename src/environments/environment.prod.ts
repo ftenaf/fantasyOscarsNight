633ffd123c80dbf017cff9c009ce934a9eea4d16
Francisco Tena <ftenaf@yahoo.es>
2020-02-06 19:30:32 +0000

    environment vars on firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,20 @@
+const functions = require('firebase-functions');
+
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: functions.config().api.key,
+  authDomain: "oscarsfantasynight.firebaseapp.com",
+  databaseURL: "https://oscarsfantasynight.firebaseio.com",
+  projectId: "oscarsfantasynight",
+  storageBucket: "oscarsfantasynight.appspot.com",
+  messagingSenderId: functions.config().messaging.senderid,
+  appId: functions.config().app.id
 };
 
 export const environment = {
-  production: true,
-  movieDbKey: '',
+  production: false,
+  movieDbKey: functions.config().moviedb.key,
   movieDbUrl: 'https://api.themoviedb.org/3',
   poster_url_prefix: 'https://image.tmdb.org/t/p/w300/',
   firebase: firebaseConfig, 
+  countryApiURL: "https://restcountries.eu/rest/v2/alpha/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,23 +1,25 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+const functions = require('firebase-functions');
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: functions.config().api.key,
+  authDomain: "oscarsfantasynight.firebaseapp.com",
+  databaseURL: "https://oscarsfantasynight.firebaseio.com",
+  projectId: "oscarsfantasynight",
+  storageBucket: "oscarsfantasynight.appspot.com",
+  messagingSenderId: functions.config().messaging.senderid,
+  appId: functions.config().app.id
 };
 
 export const environment = {
   production: false,
-  movieDbKey: '',
+  movieDbKey: functions.config().moviedb.key,
   movieDbUrl: 'https://api.themoviedb.org/3',
   poster_url_prefix: 'https://image.tmdb.org/t/p/w300/',
   firebase: firebaseConfig, 
+  countryApiURL: "https://restcountries.eu/rest/v2/alpha/"
 };
 
 /*
